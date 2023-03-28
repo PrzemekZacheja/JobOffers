f22fa4c26f4c617dab14093f7b530b10f5b9880a
PZacheja <przemek.zacheja@gmail.com>
2023-03-28 09:50:24 +0200

    Add packages, Facades class and test classes

--- src/test/java/pl/joboffers/domain/login_and_register/LoginAndRegisterFacadeTest.java
+++ src/test/java/pl/joboffers/domain/login_and_register/LoginAndRegisterFacadeTest.java
@@ -1,5 +1,13 @@
 package pl.joboffers.domain.login_and_register;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class LoginAndRegisterFacadeTest {
 
+    @Test
+    void firstTest() {
+        assertThat(1).isEqualTo(1);
+    }
 }
--- src/test/java/pl/joboffers/domain/offers/OfferFacadeTest.java
+++ src/test/java/pl/joboffers/domain/offers/OfferFacadeTest.java
@@ -1,5 +1,13 @@
 package pl.joboffers.domain.offers;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class OfferFacadeTest {
 
+    @Test
+    void firstTest() {
+        assertThat(1).isEqualTo(1);
+    }
 }
