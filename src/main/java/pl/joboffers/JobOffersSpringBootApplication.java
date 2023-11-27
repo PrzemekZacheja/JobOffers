7430b845fd098b3cad6fba38934c0caf65b125ba
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-11-27 15:47:03 +0100

    refactored OfferResponseClientConfig, deleted OfferResponseRestTemplateConfigurationProperties

--- src/main/java/pl/joboffers/JobOffersSpringBootApplication.java
+++ src/main/java/pl/joboffers/JobOffersSpringBootApplication.java
@@ -2,12 +2,9 @@ package pl.joboffers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import pl.joboffers.infrastracture.offer.http.OfferResponseRestTemplateConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({OfferResponseRestTemplateConfigurationProperties.class})
 @EnableMongoRepositories
 public class JobOffersSpringBootApplication {
 
