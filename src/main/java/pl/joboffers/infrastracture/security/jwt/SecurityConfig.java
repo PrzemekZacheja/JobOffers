fc54244c3b6b675a1de0e273666dcaa170b9c311
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-12-12 15:07:05 +0100

    added securityFilterChain() in SecurityConfig

--- src/main/java/pl/joboffers/infrastracture/security/jwt/SecurityConfig.java
+++ src/main/java/pl/joboffers/infrastracture/security/jwt/SecurityConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 import pl.joboffers.domain.login.LoginAndRegisterFacade;
 
 @Configuration
@@ -20,5 +23,39 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(LoginAndRegisterFacade loginAndRegisterFacade) {
         return new LoginUserDetailsService(loginAndRegisterFacade);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeHttpRequests()
+                .antMatchers("/swagger-ui/**")
+                .permitAll()
+                .antMatchers("/v3/api-docs")
+                .permitAll()
+                .antMatchers("/webjars/**")
+                .permitAll()
+                .antMatchers("/token/**")
+                .permitAll()
+                .antMatchers("/register/**")
+                .permitAll()
+                .antMatchers("/swagger-resources/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .httpBasic()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling();
+
+        return http.build();
     }
 }
